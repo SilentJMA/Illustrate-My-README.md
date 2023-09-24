1ec3f676048c87d70f7ca8a8ce2d141b22c91035
SilentJMA <kilwanitro@gmail.com>
2023-09-24 04:13:59 +0200

    test output

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![Make me Smile](https://youtu.be/701nyC15YkI?width=100&height=100)
+![Make me Smile](https://www.reddit.com/r/SketchDaily/comments/14181sj/june_5th_clown_propaganda/?width=100&height=100)
 
 # Smile
 Take a look at my script that fetches posts every 5 minutes and brings a smile to your face!
--- smile.py
+++ smile.py
@@ -3,7 +3,7 @@ import json
 import logging
 
 # Constants
-REDDIT_API_URL = "https://www.reddit.com/r/Art/random.json?limit=1"
+REDDIT_API_URL = "https://www.reddit.com/r/SketchDaily/random.json?limit=1"
 USER_AGENT = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
     "(KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
@@ -12,7 +12,7 @@ USER_AGENT = (
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
-def fetch_random_link(api_url, user_agent):
+def fetch_random_meme(api_url, user_agent):
     try:
         response = requests.get(api_url, headers={'User-agent': user_agent})
         response.raise_for_status()  # Raise an exception for HTTP errors (e.g., 404)
@@ -22,25 +22,25 @@ def fetch_random_link(api_url, user_agent):
     except json.JSONDecodeError as e:
         logging.error(f"JSON decoding error: {e}")
 
-def extract_link_url(post_data):
+def extract_meme_url(meme_data):
     try:
         if (
-            isinstance(post_data, list)
-            and post_data
-            and "data" in post_data[0]
-            and "children" in post_data[0]["data"]
-            and post_data[0]["data"]["children"]
-            and "data" in post_data[0]["data"]["children"][0]
-            and "url" in post_data[0]["data"]["children"][0]["data"]
+            isinstance(meme_data, list)
+            and meme_data
+            and "data" in meme_data[0]
+            and "children" in meme_data[0]["data"]
+            and meme_data[0]["data"]["children"]
+            and "data" in meme_data[0]["data"]["children"][0]
+            and "url" in meme_data[0]["data"]["children"][0]["data"]
         ):
-            link_url = post_data[0]["data"]["children"][0]["data"]["url"]
-            return f"{link_url}?width=100&height=100"
+            meme_url = meme_data[0]["data"]["children"][0]["data"]["url"]
+            return f"{meme_url}?width=100&height=100"
         else:
             logging.error("Unexpected JSON structure in the response.")
     except Exception as e:
-        logging.error(f"An error occurred while extracting link URL: {e}")
+        logging.error(f"An error occurred while extracting meme URL: {e}")
 
-def update_readme_with_link(markdown):
+def update_readme_with_meme(markdown):
     try:
         with open('README.md', 'r') as file:
             contents = file.readlines()
@@ -56,12 +56,12 @@ def update_readme_with_link(markdown):
         logging.error(f"An error occurred while updating README.md: {e}")
 
 def main():
-    post_data = fetch_random_link(REDDIT_API_URL, USER_AGENT)
-    if post_data:
-        link_url = extract_link_url(post_data)
-        if link_url:
-            markdown = f"![Make me Smile]({link_url})"
-            update_readme_with_link(markdown)
+    meme_data = fetch_random_meme(REDDIT_API_URL, USER_AGENT)
+    if meme_data:
+        meme_url = extract_meme_url(meme_data)
+        if meme_url:
+            markdown = f"![Make me Smile]({meme_url})"
+            update_readme_with_meme(markdown)
 
 if __name__ == "__main__":
     main()
