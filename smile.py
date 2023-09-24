3e306254d7db87f6fad6c369914b174fc5ce43c9
SilentJMA <kilwanitro@gmail.com>
2023-09-24 04:08:36 +0200

    test output

--- smile.py
+++ smile.py
@@ -3,7 +3,7 @@ import json
 import logging
 
 # Constants
-REDDIT_API_URL = "https://www.reddit.com/r/MakeMeSmile/random.json?limit=1"
+REDDIT_API_URL = "https://www.reddit.com/r/Art/random.json?limit=1"
 USER_AGENT = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
     "(KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
