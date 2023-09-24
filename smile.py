4edb1c39cca3a35718ada71ecba9e132e8b540dd
SilentJMA <kilwanitro@gmail.com>
2023-09-24 04:14:56 +0200

    test output

--- smile.py
+++ smile.py
@@ -3,7 +3,7 @@ import json
 import logging
 
 # Constants
-REDDIT_API_URL = "https://www.reddit.com/r/SketchDaily/random.json?limit=1"
+REDDIT_API_URL = "https://www.reddit.com/r/DigitalPainting/random.json?limit=1"
 USER_AGENT = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
     "(KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
