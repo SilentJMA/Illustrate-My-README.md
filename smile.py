0f30f11a82b4ceff24637d69b1382c05c625ce1a
SilentJMA <kilwanitro@gmail.com>
2023-09-24 04:05:11 +0200

    test output

--- smile.py
+++ smile.py
@@ -1,65 +1,84 @@
+#  Copyright (c) 2023 Mohamed Ayoub Jabane, SilentJMA on GitHub
 import requests
 import json
-import logging
+import re
 
-# Constants
-REDDIT_API_URL = "https://www.reddit.com/r/MakeMeSmile/random.json?limit=1"
-USER_AGENT = (
+# Define the API endpoint to fetch memes
+reddit_api_url = "https://www.reddit.com/r/MakeMeSmile/random.json?limit=1"
+
+# Define a user agent to mimic a web browser
+user_agent = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
     "(KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
 )
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-
 def fetch_random_meme(api_url, user_agent):
     try:
+        # Make a request to the Reddit API with the specified user agent
         response = requests.get(api_url, headers={'User-agent': user_agent})
-        response.raise_for_status()  # Raise an exception for HTTP errors (e.g., 404)
-        return json.loads(response.text)
-    except requests.exceptions.RequestException as e:
-        logging.error(f"Request error: {e}")
-    except json.JSONDecodeError as e:
-        logging.error(f"JSON decoding error: {e}")
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            meme_data = json.loads(response.text)
+            return meme_data
+        else:
+            print(f"Failed to fetch meme. Status code: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"An error occurred while fetching the meme: {str(e)}")
+        return None
 
 def extract_meme_url(meme_data):
     try:
+        # Extract meme URL from the JSON response
         if (
             isinstance(meme_data, list)
-            and meme_data
+            and len(meme_data) > 0
+            and isinstance(meme_data[0], dict)
             and "data" in meme_data[0]
             and "children" in meme_data[0]["data"]
-            and meme_data[0]["data"]["children"]
+            and len(meme_data[0]["data"]["children"]) > 0
             and "data" in meme_data[0]["data"]["children"][0]
             and "url" in meme_data[0]["data"]["children"][0]["data"]
         ):
             meme_url = meme_data[0]["data"]["children"][0]["data"]["url"]
-            return f"{meme_url}?width=100&height=100"
+            return meme_url + "?width=100&height=100"  # Add query string to reduce image size
         else:
-            logging.error("Unexpected JSON structure in the response.")
+            print("Unexpected JSON structure in the response.")
+            return None
     except Exception as e:
-        logging.error(f"An error occurred while extracting meme URL: {e}")
+        print(f"An error occurred while extracting meme URL: {str(e)}")
+        return None
 
 def update_readme_with_meme(markdown):
     try:
+        # Read the existing contents of README.md
         with open('README.md', 'r') as file:
             contents = file.readlines()
 
+        # Find the index of the first line that contains "![Make me Smile]"
+        index_to_replace = None
         for i, line in enumerate(contents):
             if "![Make me Smile]" in line:
-                contents[i] = markdown + "\n"
+                index_to_replace = i
                 break
 
+        # If a line with "![Make me Smile]" was found, replace it
+        if index_to_replace is not None:
+            contents[index_to_replace] = markdown + "\n"
+
+        # Write the modified contents back to README.md
         with open('README.md', 'w') as file:
             file.writelines(contents)
     except Exception as e:
-        logging.error(f"An error occurred while updating README.md: {e}")
+        print(f"An error occurred while updating README.md: {str(e)}")
 
 def main():
-    meme_data = fetch_random_meme(REDDIT_API_URL, USER_AGENT)
+    meme_data = fetch_random_meme(reddit_api_url, user_agent)
     if meme_data:
         meme_url = extract_meme_url(meme_data)
         if meme_url:
+            # Create a Markdown image link with the meme URL
             markdown = f"![Make me Smile]({meme_url})"
             update_readme_with_meme(markdown)
 
